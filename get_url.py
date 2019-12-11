41e24959e9d557293431d6f08217881ea3e2ffb6
yijie <zhangyj@kilox.cn>
2019-12-11 14:38:01 +0800

    add more

--- get_url.py
+++ get_url.py
@@ -2,172 +2,50 @@ import requests, json
 import pandas  as pd
 from tqdm import tqdm
 import math
-from tes_selenium import get_tracking_num, get_tracking_num_SF_100
+from tes_selenium import TestSelenium
 
 
-def DHL(trackingNumber):
-    params = {'trackingNumber': str(trackingNumber),
-              'language': 'zh',
-              'requesterCountryCode': 'CN'
-              }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+class ExcelProcess:
+    def __init__(self):
+        self.ts = TestSelenium()
+        pass
+    
+    def process(self, in_out_file):
+        df = pd.read_excel(in_out_file)
+        
+        times = df['签收时间']
+        
+        valuse = df.ix[:, ['快递公司', '物流单号', '手机号后四位', '签收时间']].values
+        
+        current_cnt = 0
+        for index, data in enumerate(tqdm(valuse)):
+            company, tracking_number, phone_number, receive_time = data
+            tracking_number = str(tracking_number)
+            tracking_number = tracking_number.strip('\t')
+            phone_number = str(phone_number)
+            
+            ok = False
+            if isinstance(receive_time, float):
+                if math.isnan(receive_time):
+                    time = ''
+                    if company != '顺丰标准到付' and company != '顺丰标准快递' and company != '顺丰国际':
+                        ok, time = self.ts.get_tracking_num_SF_100(tracking_number, phone_number)
+                    if company == 'FedEx':
+                        ok, time = self.ts.get_tracking_num_DHL(tracking_number)
+                    if company == 'DHL国际快递':
+                        ok, time = self.ts.get_tracking_num_DHL(tracking_number)
+                    
+                    if ok:
+                        times[index] = time
+                        current_cnt += 1
+            if current_cnt >= 10:
+                df.to_excel(in_out_file, index=False, header=True)
+                current_cnt = 0
+                print('************* 保存一次 *************')
+        df.to_excel(in_out_file, index=False, header=True)
 
-    }
-    url = 'https://www.logistics.dhl/utapi'
-
-    try:
-        r = requests.get(url, params=params, headers=headers).json()
-
-        arrive_time = r['shipments'][0]['status']['timestamp']
-        print('DHL {} 时间 {}'.format(trackingNumber, arrive_time))
-    except KeyError:
-        return 0
-    return arrive_time
-
-
-def FedEx(trackingNumber='129335520272'):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-
-    }
-    url = 'https://www.fedex.com/trackingCal/track'
-    data = json.loads(
-        '{"TrackPackagesRequest":{"appType":"WTRK","appDeviceType":"","supportHTML":true,"supportCurrentLocation":true,"uniqueKey":"","processingParameters":{},"trackingInfoList":[{"trackNumberInfo":{"trackingNumber":"129335520272","trackingQualifier":"","trackingCarrier":""}}]}}')
-    data['TrackPackagesRequest']['trackingInfoList'][0]['trackNumberInfo']['trackingNumber'] = trackingNumber
-    params = {'data': data, 'action': 'trackpackages', 'locale': 'zh_CN', 'version': 1, 'format': 'json'}
-    r = requests.post(url, params=params, headers=headers).json()
-    # a = r
-    try:
-        arrive_time = r['TrackPackagesResponse']['packageList'][0]['displayActDeliveryDateTime']
-        print('FedEx {} 时间 {}'.format(trackingNumber, arrive_time))
-    except KeyError:
-        return 0
-    return arrive_time
-
-
-# FedEx()
-
-
-def Express100(trackingNumber='129335520272'):
-    params = {'postid': str(trackingNumber),
-              # 'language': 'zh',
-              # 'requesterCountryCode': 'CN'
-              'type': 'fedex',
-              'id': 4,
-              'valicode': '',
-              'tmp': 0.7895764382626502,
-              'temp': 0.08769087120054486,
-              'platform': 'MWWW'
-              }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-        'Referer': 'https://www.kuaidi100.com/frame/bing/?iframeId=2699975952',
-        'Host': 'www.kuaidi100.com'
-        # 'Cookie': 'csrftoken=eEbp5_bqpIQegUaMY66y8dgKTu7Vjz5O5HNRm_lOGbo; WWWID=WWW6977E6C32CACDFEF8EF96C786895B39B; Hm_lvt_22ea01af58ba2be0fec7c11b25e88e6c=1575766316; Hm_lpvt_22ea01af58ba2be0fec7c11b25e88e6c=1575766316; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1'
-    }
-    # url = 'https://www.kuaidi100.com/query'
-    url = 'https://m.kuaidi100.com/query'
-    try:
-        r = requests.get(url, params=params, headers=headers).json()
-
-        a = r
-        arrive_time = r['shipments'][0]['status']['timestamp']
-        print('DHL {} 时间 {}'.format(trackingNumber, arrive_time))
-    except KeyError:
-        return 0
-    return arrive_time
-
-
-# Express100()
-
-# 527233538612869650	顺丰标准到付	"235609499749	"	18503089505	9505
-def parseDHL():
-    df = pd.read_excel('result.xlsx')
-
-    times = df['签收时间']
-
-    valuse = df.ix[:, ['快递公司', '物流单号', '手机号后四位', '签收时间']].values
-
-    current_cnt = 0
-    for index, data in enumerate(tqdm(valuse)):
-        current_cnt += 1
-
-        company, tracking_number, phone_number, receive_time = data
-        tracking_number = str(tracking_number)
-        tracking_number = tracking_number.strip('\t')
-
-        if company != 'DHL国际快递':
-            continue
-
-        elif isinstance(receive_time, float):
-            if math.isnan(receive_time):
-                time = DHL(tracking_number)
-                times[index] = time
-            # print('phone number is ', phone_number)
-
-            # times[index] = get_tracking_num(tracking_number)
-        # if company == 'FedEx':
-        #     times[index] = FedEx(tracking_number)
-
-        # 每5个保存一次文件
-    df.to_excel('result.xlsx', index=False, header=True)
-
-
-def parse2():
-    df = pd.read_excel('out1.xlsx')
-
-    times = df['签收时间']
-
-    valuse = df.ix[:, ['快递公司', '物流单号', '手机号后四位', '签收时间']].values
-
-    current_cnt = 0
-    for index, data in enumerate(tqdm(valuse)):
-        current_cnt += 1
-        if index < 4362:
-            continue
-
-        company, tracking_number, phone_number, receive_time = data
-        tracking_number = str(tracking_number)
-        tracking_number = tracking_number.strip('\t')
-
-        if company != '顺丰标准到付' and company != '顺丰标准快递' and company != '顺丰国际':
-            continue
-
-        elif isinstance(receive_time, float):
-            if math.isnan(receive_time):
-                ok, time = get_tracking_num_SF_100(tracking_number, str(phone_number))
-                if ok:
-                    times[index] = time
-            # print('phone number is ', phone_number)
-
-            # times[index] = get_tracking_num(tracking_number)
-        # if company == 'FedEx':
-        #     times[index] = FedEx(tracking_number)
-
-        # 每5个保存一次文件
-        if current_cnt >= 10:
-            df.to_excel('out2.xlsx', sheet_name='Sheet2', index=False, header=True)
-            current_cnt = 0
-            print('保存一次文件 当前已保存', index)
-    df.to_excel('out2.xlsx', sheet_name='Sheet2', index=False, header=True)
-
-    # 销售单号
-    # 快递公司
-    # 物流单号
-    # 收货人手机
-    # 手机号后四位
-    # 签收时间
-    # 网址
-
-
-parse2()
 
 if __name__ == "__main__":
-    # while True:
-    #     main()
-    #     i = input("是否继续查询?(任意字符为继续,q为退出)")
-    #     if i == "q":
-    #         print("感谢使用,祝您生活愉快,再见!")
-    #         break
+    ep = ExcelProcess()
+    ep.process('result.xlsx')
     pass
--- tes_selenium.py
+++ tes_selenium.py
@@ -1,5 +1,5 @@
+import requests
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
@@ -9,178 +9,122 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-# from selenium.web
-# chrome = webdriver.Chrome()
-
 chrome_opt = Options()  # 创建参数设置对象.
 chrome_opt.add_argument('--headless')  # 无界面化.
 chrome_opt.add_argument('--disable-gpu')  # 配合上面的无界面化.
 chrome_opt.add_argument('--window-size=1366,768')  # 设置窗口大小, 窗口大小会有影响.
 
-# 创建Chrome对象并传入设置信息.
-# browser = webdriver.Chrome(chrome_options=chrome_opt)
-browser = webdriver.Chrome()
 
-browser.get('https://www.kuaidi100.com')
-# browser.get('file:///home/kilox/Downloads/test.html')
-# browser.get('http://www.kuaidiwo.cn/shunfeng.html')
-input_bar = browser.find_element_by_name("postid")
-
-
-# browser.get('https://www.sf-express.com/cn/sc/dynamic_function/waybill/#search/bill-number/')
-
-# input_bar = WebDriverWait(browser, 20).until(
-#     EC.presence_of_element_located((By.CLASS_NAME, "token-input"))
-# )
-def get_visible_element(xpath):
-    try:
-        closer_btn = WebDriverWait(browser, 1).until(
-            EC.visibility_of_element_located((By.XPATH, xpath))
-        )
-    except:
-        return None
+class TestSelenium:
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.browser_current = None
     
-    return closer_btn
-
-
-def get_tracking_num_SF_100(tracking_number, phone_number):
-    if phone_number.find('*') != -1:
-        return False, 0
-    if phone_number.find('.') != -1:
-        return False, 0
-    if phone_number == '':
-        return False, 0
-    try:
-        check_btn = get_visible_element('/html/body/div[7]/div[2]/div/a')
-        if check_btn is not None:
-            check_btn.click()
+    def get_visible_element(self, xpath):
+        try:
+            closer_btn = WebDriverWait(self.browser, 1).until(
+                EC.visibility_of_element_located((By.XPATH, xpath))
+            )
+        except:
+            return None
         
-        input_bar.click()
-        input_bar.clear()
-        input_bar.send_keys(tracking_number)
-        
-        ensure_btn = browser.find_element_by_id('query')
-        ensure_btn.click()
-        
-        # import time
-        # time.sleep(3)
-        
-        input = WebDriverWait(browser, 2).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="checkCode"]/div[2]/div/div[2]/input[1]'))
-            # EC.visibility_of_element_located
-        )
-        
-        # input = WebDriverWait(browser, 10).until(
-        #     EC.presence_of_element_located((By.XPATH, '//*[@id="checkCode"]/div[2]/div/div[2]/input[1]'))
-        # )
-        # input = browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[1]')
-        # input = verify_code.find_element_by_xpath('//input[3]')
-        ActionChains(browser).send_keys_to_element(input, phone_number[0]).perform()
-        input = browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[2]')
-        ActionChains(browser).send_keys_to_element(input, phone_number[1]).perform()
-        input = browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[3]')
-        ActionChains(browser).send_keys_to_element(input, phone_number[2]).perform()
-        input = browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[4]')
-        ActionChains(browser).send_keys_to_element(input, phone_number[3]).perform()
-        
-        ensure_btn = browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[3]')
-        ensure_btn.click()
-        
-        # time.sleep(3)
-        
-        date = WebDriverWait(browser, 3).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="queryResult"]/div[3]/div[2]/table/tbody/tr[1]/td[1]'))
-        )
-        print(date.text)
-        return True, date.text
-    except (TimeoutException, NoSuchElementException, ElementNotVisibleException, WebDriverException, TypeError):
-        print('无法获取')
-        # print(browser.page_source)
-        return False, 0
+        return closer_btn
     
-    # verify_code.click()
-    # verify_code.send_keys('1234')
-    
-    # input_bar.clear()
-
-
-def get_tracking_num_SF(tracking_number):
-    input_bar.click()
-    input_bar.clear()
-    input_bar.send_keys(tracking_number)
-    
-    ensure_btn = browser.find_element_by_id('queryBill')
-    ensure_btn.click()
-    
-    try:
-        # import time
-        # time.sleep(3)
-        iframe = browser.find_element_by_xpath('//frame')  # 找到“嵌套”的iframe
-        ts = browser.find_element_by_id('tcaptcha_window')
-        # iframe = browser.find_element_by_xpath('//iframe')  # 找到“嵌套”的iframe
-        browser.switch_to.frame('tcaptcha_popup')  # 切换到iframe
-        ts = browser.find_element_by_class_name('tcaptcha_drag_button')
+    def get_presence_element(self, xpath):
+        try:
+            closer_btn = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+        except:
+            return None
         
-        verify_code = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "tcaptcha_drag_button"))
-        )
-        # source_element = browser.find_element_by_id('tcaptcha_drag_thumb')
-        
-        # 27.5 ~ 228.action = ActionChains(driver)            # 实例化一个action对象
-        # action.click_and_hold(button).perform()  # perform()用来执行ActionChains中存储的行为
-        # action.reset_actions()
-        # action.move_by_offset(180, 0).perform()  # 移动滑块5
-        ActionChains(browser).click_and_hold(verify_code).perform()
-        ActionChains(browser).reset_actions().drag_and_drop_by_offset(verify_code, 200, 0).perform()  # 链式用法
-        
-        result_info = WebDriverWait(browser, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "result-info2"))
-        )
-        # table_tr_list = result_info.find_elements_(By.TAG_NAME, "tr")
-        
-        table_tr_list = result_info.find_elements_by_class_name('row1')
-        # result_info = browser.find_element_by_class_name('result-info')
-        last_row = result_info.find_element_by_class_name('last')
-        row_1 = last_row.find_element_by_class_name('row1')
-        day = row_1.find_element_by_class_name('day')
-        time = row_1.find_element_by_class_name('time')
-        total_time = day.text + ' T ' + time.text
-        print(tracking_number, '时间为{}'.format(total_time))
-        return True, total_time
-    except (TimeoutException, NoSuchElementException):
-        print('无法获取')
-        print(browser.page_source)
-        return False, 0
-
-
-def get_tracking_num(tracking_number):
-    input_bar.click()
-    input_bar.clear()
-    input_bar.send_keys(tracking_number)
+        return closer_btn
     
-    ensure_btn = browser.find_element_by_id('query')
-    ensure_btn.click()
-    
-    try:
-        result_info = WebDriverWait(browser, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "result-info"))
-        )
+    def get_tracking_num_DHL(self, tracking_number):
+        params = {'trackingNumber': str(tracking_number),
+                  'language': 'zh',
+                  'requesterCountryCode': 'CN'
+                  }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+            
+        }
+        url = 'https://www.logistics.dhl/utapi'
         
-        # result_info = browser.find_element_by_class_name('result-info')
-        last_row = result_info.find_element_by_class_name('last')
-        row_1 = last_row.find_element_by_class_name('row1')
-        day = row_1.find_element_by_class_name('day')
-        time = row_1.find_element_by_class_name('time')
-        total_time = day.text + ' T ' + time.text
-        print(tracking_number, '时间为{}'.format(total_time))
-        return True, total_time
-    except (TimeoutException, NoSuchElementException):
-        print('无法获取')
-        return False, 0
-
-
-# DH.20190930.0642-04	顺丰标准快递	"SF1160423969219	"	13522005547	5547
-
-# print(browser.page_source)
-if __name__ == '__main__':
-    get_tracking_num_SF_100('SF1160423969219', list('5547'))
+        try:
+            r = requests.get(url, params=params, headers=headers).json()
+            
+            arrive_time = r['shipments'][0]['status']['timestamp']
+            print('DHL {} 时间 {}'.format(tracking_number, arrive_time))
+        except KeyError:
+            return 0
+        return arrive_time
+    
+    def get_tracking_num_FedEx(self, tracking_number):
+        try:
+            if self.browser_current != 'https://www.kuaidi100.com/global/fedexus.shtml?from=openv':
+                self.browser_current = 'https://www.kuaidi100.com/global/fedexus.shtml?from=openv'
+                self.browser.get(self.browser_current)
+            input_bar = self.browser.find_element_by_name("postid")
+            input_bar.click()
+            input_bar.clear()
+            input_bar.send_keys(tracking_number)
+            
+            ensure_btn = self.browser.find_element_by_id('query')
+            ensure_btn.click()
+            
+            date = self.get_presence_element('/html/body/div[3]/div[3]/div[1]/div[6]/table/tbody/tr[1]/td[1]')
+            time = date.text
+            return True, time
+        except:
+            return False, 0
+    
+    def get_tracking_num_SF_100(self, tracking_number, phone_number):
+        if phone_number.find('*') != -1:
+            return False, 0
+        if phone_number.find('.') != -1:
+            return False, 0
+        if phone_number == '':
+            return False, 0
+        try:
+            if self.browser_current != 'http://www.kuaidiwo.cn/shunfeng.html':
+                self.browser_current = 'http://www.kuaidiwo.cn/shunfeng.html'
+                self.browser.get('http://www.kuaidiwo.cn/shunfeng.html')
+            
+            check_btn = self.get_visible_element('/html/body/div[7]/div[2]/div/a')
+            if check_btn is not None:
+                check_btn.click()
+            
+            input_bar = self.browser.find_element_by_name("postid")
+            
+            input_bar.click()
+            input_bar.clear()
+            input_bar.send_keys(tracking_number)
+            
+            ensure_btn = self.browser.find_element_by_id('query')
+            ensure_btn.click()
+            
+            input = WebDriverWait(self.browser, 2).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="checkCode"]/div[2]/div/div[2]/input[1]'))
+            )
+            
+            ActionChains(self.browser).send_keys_to_element(input, phone_number[0]).perform()
+            input = self.browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[2]')
+            ActionChains(self.browser).send_keys_to_element(input, phone_number[1]).perform()
+            input = self.browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[3]')
+            ActionChains(self.browser).send_keys_to_element(input, phone_number[2]).perform()
+            input = self.browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[2]/input[4]')
+            ActionChains(self.browser).send_keys_to_element(input, phone_number[3]).perform()
+            
+            ensure_btn = self.browser.find_element_by_xpath('//*[@id="checkCode"]/div[2]/div/div[3]')
+            ensure_btn.click()
+            
+            date = WebDriverWait(self.browser, 3).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//*[@id="queryResult"]/div[3]/div[2]/table/tbody/tr[1]/td[1]'))
+            )
+            print(date.text)
+            return True, date.text
+        except (TimeoutException, NoSuchElementException, ElementNotVisibleException, WebDriverException, TypeError):
+            print('无法获取')
+            return False, 0
